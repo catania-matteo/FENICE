3d38c2e18421b20afde03bf3ffb498b7ab456437
catania-matteo <117747015+catania-matteo@users.noreply.github.com>
2025-09-04 14:27:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FENICE",
+    version="0.1.0",
+    author="Matteo Catania",
+    description="My research scripts + data (depends on patched oemof fork)",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # point to your forked oemof and branch:
+        "oemof @ git+https://github.com/catania-matteo/oemof.git@fix-for-myproject",
+        "pandas>=1.0",
+        "numpy>=1.20",
+    ],
+    package_data={"FENICE": ["data/*.xlsx"]},
+)
